910544d725c5a9b245e890e2e3d6173d8b87b8f1
Nightlights524 <41940695+Nightlights524@users.noreply.github.com>
2020-12-14 14:08:07 -0600

    Correct POST path

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use('/api/shorturl', bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.post('/api/shorturl/new', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
