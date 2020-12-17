41bb47d34c102750d501583ef649b0ecfce72acf
Nightlights524 <Nightlights524@users.noreply.github.com>
2020-12-17 10:15:57 -0600

    add nanoid

--- server.js
+++ server.js
@@ -2,7 +2,22 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dns = require('dns');
+const nanoid = require('nanoid').nanoid;
 const app = express();
+
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.once('open', () => {console.log('Successfully connected');});
+// db.once('open', () => {console.log(mongoose.connection.readyState);});
+
+const urlSchema = new mongoose.Schema({
+  "original_url": String,
+  "short_url": String
+});
+
+const Url = mongoose.model('Url', urlSchema);
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -17,10 +32,11 @@ app.get('/', function(req, res) {
 
 // Your first API endpoint
 app.post('/api/shorturl/new', function(req, res) {
-  console.log(req.body);
+  // dns.lookup(req.body.url);
+
   res.json({
     original_url: req.body.url,
-    short_url: "24"
+    short_url: nanoid(5)
   });
 });
 
